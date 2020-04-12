445822889153feaef5bc456e32da657335c7101c
toshihirokato <nanamin.104@icloud.com>
2020-04-13 03:21:25 +0900

    firebase の読み取り

--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzXHtzugTt8HsHjd4JvPNLJ0BY22wo7hA",
+  authDomain: "gf-todo-app-697c4.firebaseapp.com",
+  databaseURL: "https://gf-todo-app-697c4.firebaseio.com",
+  projectId: "gf-todo-app-697c4",
+  storageBucket: "gf-todo-app-697c4.appspot.com",
+  messagingSenderId: "130859006202",
+  appId: "1:130859006202:web:d1aefd53eae806891fb143"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export const db = firebase.firestore()
+
 new Vue({
   router,
   render: h => h(App)
