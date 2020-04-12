9c19691c5ce78212067f8200b36c2d2a4c3388d8
toshihirokato <nanamin.104@icloud.com>
2020-04-13 04:34:48 +0900

    Googleアカウントでのログインポップアウト表示

---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>Googleアカウントでログインしてください</h3>
+    <button type="button" @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+export default {
+  methods: {
+    login: function() {
+      // Googleプロバイダオブジェクトのインスタンスを生成し、signInWithPopupメソッドに渡す
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithPopup(provider).then((result) => {
+        // ログイン後は、Vue Router の $router.pushメソッドを使用して Topページに遷移する
+        this.$router.push('/')
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
